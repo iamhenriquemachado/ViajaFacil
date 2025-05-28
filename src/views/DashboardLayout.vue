67cbe61186e502a8968cc182099a27559a19473b
henrique.castro <heyhenriquecastro@gmail.com>
2025-05-28 20:26:51 -0300

    add frontend project in vuejs

---
+++ src/views/DashboardLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="display: flex;">
+    <SideBarMenu />
+    <div style="flex: 1; padding: 20px;">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SideBarMenu from '../components/SideBarMenu.vue'
+</script>
